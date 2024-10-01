b5920cf6c5b0a6b92cd5e30ab55aa87c240b4398
Poll Adhikary <91479117+plk449@users.noreply.github.com>
2024-10-01 11:58:13 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(cors({
   origin: 'https://e-commerce-frontend-henna-two.vercel.app/',
-  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   // credentials: true
 }));
 
